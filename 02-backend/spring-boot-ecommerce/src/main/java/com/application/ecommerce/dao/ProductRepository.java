b8b55edb6d57c7e6717828d8b8d6666fa3a73649
Fokhrul Islam Parvez <parvezdiu16@gmail.com>
2020-08-17 02:44:55 +0600

    import @CrossOrigin Package

--- 02-backend/spring-boot-ecommerce/src/main/java/com/application/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/application/ecommerce/dao/ProductRepository.java
@@ -3,6 +3,7 @@ package com.application.ecommerce.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.application.ecommerce.entity.Product;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product,Long> {
