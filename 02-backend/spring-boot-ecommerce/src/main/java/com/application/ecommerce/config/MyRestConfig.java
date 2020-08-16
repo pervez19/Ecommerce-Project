93bdd1c05a5267907c0320c71e0efca8471a807d
Fokhrul Islam Parvez <parvezdiu16@gmail.com>
2020-08-16 18:25:39 +0600

    Configure Unsupported HttpMthod action

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/application/ecommerce/config/MyRestConfig.java
@@ -0,0 +1,27 @@
+package com.application.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+import com.application.ecommerce.entity.Product;
+import com.application.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions={HttpMethod.DELETE,HttpMethod.POST,HttpMethod.PUT};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+               .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        config.getExposureConfiguration()
+                .forDomainType( ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
+}
