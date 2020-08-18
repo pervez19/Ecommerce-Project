d4794e768e729d12794f3dd7654c923ba3c61368
Fokhrul Islam Parvez <parvezdiu16@gmail.com>
2020-08-18 14:11:54 +0600

    "first commit"

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+	private sku:string ;
+	private name:string;
+	private description:string;
+	private unitPrice:number;
+	private imageUrl:string;
+	private active:boolean;
+	private unitInStock:number;
+	private dateCreated:Date;
+	private lastUpdate:Date;
+}
