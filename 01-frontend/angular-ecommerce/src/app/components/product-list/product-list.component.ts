0fef46b649f83d602af91e4ff8aee58309726c61
Fokhrul Islam Parvez <parvezdiu16@gmail.com>
2020-08-20 10:02:32 +0600

    Product Search by Category

--- 01-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 01-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
- // templateUrl: './product-list-table.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products:Product[];
+  currentCategoryId:number;
 
-  constructor(private productService: ProductService) { }
+
+  constructor(private productService: ProductService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {this.listProducts();});
   }
   listProducts()
   {
-    this.productService.getProductList()
+    const hasCategoryId:boolean=this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId)
+    {
+      this.currentCategoryId=+this.route.snapshot.paramMap.get('id');
+    }
+    else
+    {
+      this.currentCategoryId=1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId)
     .subscribe(data => {this.products=data;}
     )
   }
